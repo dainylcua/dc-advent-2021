2a5d92de022e131dfb31e14886612af4831c0136
dainylcua <dainylcua@gmail.com>
2021-12-06 00:56:49 -0600

    Day 6 Complete

--- day6.js
+++ day6.js
@@ -1,6 +1,6 @@
 // const fish = [2,4,1,5,1,3,1,1,5,2,2,5,4,2,1,2,5,3,2,4,1,3,5,3,1,3,1,3,5,4,1,1,1,1,5,1,2,5,5,5,2,3,4,1,1,1,2,1,4,1,3,2,1,4,3,1,4,1,5,4,5,1,4,1,2,2,3,1,1,1,2,5,1,1,1,2,1,1,2,2,1,4,3,3,1,1,1,2,1,2,5,4,1,4,3,1,5,5,1,3,1,5,1,5,2,4,5,1,2,1,1,5,4,1,1,4,5,3,1,4,5,1,3,2,2,1,1,1,4,5,2,2,5,1,4,5,2,1,1,5,3,1,1,1,3,1,2,3,3,1,4,3,1,2,3,1,4,2,1,2,5,4,2,5,4,1,1,2,1,2,4,3,3,1,1,5,1,1,1,1,1,3,1,4,1,4,1,2,3,5,1,2,5,4,5,4,1,3,1,4,3,1,2,2,2,1,5,1,1,1,3,2,1,3,5,2,1,1,4,4,3,5,3,5,1,4,3,1,3,5,1,3,4,1,2,5,2,1,5,4,3,4,1,3,3,5,1,1,3,5,3,3,4,3,5,5,1,4,1,1,3,5,5,1,5,4,4,1,3,1,1,1,1,3,2,1,2,3,1,5,1,1,1,4,3,1,1,1,1,1,1,1,1,1,2,1,1,2,5,3]
 const fish = [3,4,3,1,2]
-for(i=0; i<256; i++) {
+for(i=0; i<80; i++) {
   let newFish = 0
   fish.forEach((cycle, idx) => {
     if(cycle === 0) {
@@ -12,6 +12,55 @@ for(i=0; i<256; i++) {
     }
   })
 }
-console.log(fish.length)
+// console.log(fish.length)
 // Part 1 = 362666
 // Part 1 code leads to overflow in Part 2
+// Instead, track amount of fish by tracking days of breeding
+
+// Part 2
+// Object instead?
+const fish2 = [2,4,1,5,1,3,1,1,5,2,2,5,4,2,1,2,5,3,2,4,1,3,5,3,1,3,1,3,5,4,1,1,1,1,5,1,2,5,5,5,2,3,4,1,1,1,2,1,4,1,3,2,1,4,3,1,4,1,5,4,5,1,4,1,2,2,3,1,1,1,2,5,1,1,1,2,1,1,2,2,1,4,3,3,1,1,1,2,1,2,5,4,1,4,3,1,5,5,1,3,1,5,1,5,2,4,5,1,2,1,1,5,4,1,1,4,5,3,1,4,5,1,3,2,2,1,1,1,4,5,2,2,5,1,4,5,2,1,1,5,3,1,1,1,3,1,2,3,3,1,4,3,1,2,3,1,4,2,1,2,5,4,2,5,4,1,1,2,1,2,4,3,3,1,1,5,1,1,1,1,1,3,1,4,1,4,1,2,3,5,1,2,5,4,5,4,1,3,1,4,3,1,2,2,2,1,5,1,1,1,3,2,1,3,5,2,1,1,4,4,3,5,3,5,1,4,3,1,3,5,1,3,4,1,2,5,2,1,5,4,3,4,1,3,3,5,1,1,3,5,3,3,4,3,5,5,1,4,1,1,3,5,5,1,5,4,4,1,3,1,1,1,1,3,2,1,2,3,1,5,1,1,1,4,3,1,1,1,1,1,1,1,1,1,2,1,1,2,5,3]
+// const fish2 = [3,4,3,1,2]
+const fishObj = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0
+}
+fish2.forEach((fish) => {
+  fishObj[fish]++
+})
+for(i=0; i<256; i++) {
+  let newFish = 0
+  for(day in fishObj) {
+    let days = parseInt(day)
+    // If a lanternfish gives birth
+    if(days === 0) {
+      // Track the number of new fish
+      newFish = fishObj[days]
+      // Day 1 fish are now day 0 fish
+      fishObj[days] = fishObj[days+1]
+    // Special case: Lanternfish on day 6
+    } else if(days === 6) {
+      // Set the amount of fish to the next day's fish, but also add the fish that gave birth
+      fishObj[days] = fishObj[days+1] + newFish
+    // For lanternfish that are born
+    } else if(days === 8) {
+      fishObj[days] = newFish
+    // For other fish, simply age them by 1 day
+    } else {
+      fishObj[days] = fishObj[days+1]
+    }
+  }
+}
+
+let fishTot = 0
+for(day in fishObj) {
+  fishTot += fishObj[day]
+}
+console.log(fishTot)
